55aaf31d203d8fa0932ee2b0013498abedbebf89
hydra <me74242@gmail.com>
2022-12-09 09:55:28 +0330

    more Complex Promise

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 "use strict";
+const users = [
+    {
+        userId: 1,
+        userName: "hydra 1",
+    },
+    {
+        userId: 2,
+        userName: "hydra 2",
+    },
+    {
+        userId: 3,
+        userName: "hydra 3",
+    },
+];
+function getUserData(userId) {
+    return users.find((item) => item.userId === userId)
+        ? users.find((item) => item.userId === userId)
+        : false;
+}
+function getUserRepositories(user) {
+    // get From GITHUB API
+    // ..
+    // ..
+    const userRepos = ["repo1", "repo2", "repo3"];
+    console.log(`for user ${user.userId} - save ${userRepos.length} repos - repos are : ${userRepos}`);
+    return userRepos;
+}
 const promise = new Promise((resolve, reject) => {
-    const err = false;
-    if (!err) {
-        resolve("Done !");
+    const user = getUserData(2);
+    if (user) {
+        resolve(user);
     }
-    else
-        [reject(new Error("Error Msg !"))];
+    else {
+        reject(new Error("selected User Not Found !"));
+    }
 });
 promise
     .then((result) => {
--- index.ts
+++ index.ts
@@ -1,8 +1,49 @@
+type userId_Type = string | number;
+
+interface I_User {
+  userId: userId_Type;
+  userName: string;
+}
+
+const users: I_User[] = [
+  {
+    userId: 1,
+    userName: "hydra 1",
+  },
+  {
+    userId: 2,
+    userName: "hydra 2",
+  },
+  {
+    userId: 3,
+    userName: "hydra 3",
+  },
+];
+
+function getUserData(userId: userId_Type) {
+  return users.find((item) => item.userId === userId)
+    ? users.find((item) => item.userId === userId)
+    : false;
+}
+
+function getUserRepositories(user: I_User) {
+  // get From GITHUB API
+  // ..
+  // ..
+  const userRepos: string[] = ["repo1", "repo2", "repo3"];
+  console.log(
+    `for user ${user.userId} - save ${userRepos.length} repos - repos are : ${userRepos}`
+  );
+  return userRepos;
+}
+
 const promise = new Promise((resolve: Function, reject: Function) => {
-  const err: boolean = false;
-  if (!err) {
-    resolve("Done !");
-  } else [reject(new Error("Error Msg !"))];
+  const user = getUserData(2);
+  if (user) {
+    resolve(user);
+  } else {
+    reject(new Error("selected User Not Found !"));
+  }
 });
 
 promise
